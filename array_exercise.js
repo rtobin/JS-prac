7b92a458f91971b0f67fb7a8f56ba0b423f8a3a0
JimmyRyan <JimmyRyan@email>
2015-10-29 12:21:15 -0400

    Finished Array exercises.

--- array_exercise.js
+++ array_exercise.js
@@ -1,10 +1,49 @@
+"use strict";
+
+var array = [1,2,3];
+var matrix = [[1,2,3], [4,5,6], [7,8,9]];
+
+
 Array.prototype.myUniq = function(){
   var arr = []
-  var inArr = function(el, idx, array){
+  var inArr = function(el){
     if (arr.indexOf(el) === -1) {
       arr.push(el)
     }
   }
   this.forEach(inArr)
   return arr
+}
+
+Array.prototype.twoSum = function(target) {
+
+    for(var i = 0; i < this.length; i++){
+
+      for(var j = i+1; j < this.length; j++){
+          if (this[i]+this[j] === target) {
+            return [i,j];
+          };
+    };
+  };
+  return -0;
+}
+
+Array.prototype.myTranspose = function() {
+
+  var transposed = [];
+
+    for(var j = 0; j < this.length; j++){
+
+      var row = [];
+
+      for(var i = 0; i < this[0].length; i++) {
+
+        row.push(this[i][j]);
+
+      }
+      transposed.push(row);
+
+
+    }
+    return transposed;
 }
