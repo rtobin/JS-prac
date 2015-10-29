8a2845391b7888715fe556dd41c114c798154829
JimmyRyan <JimmyRyan@email>
2015-10-29 15:32:53 -0400

    Do Iteration exercises.

--- iteration_exercises.js
+++ iteration_exercises.js
@@ -23,3 +23,17 @@ Array.prototype.bubbleSort = function() {
   return this;
 
 }
+
+
+String.prototype.mySubstrings = function() {
+  var res = [];
+  for(var i = 0; i < this.length; i++) {
+    for(var j = i + 1; j<= this.length; j++) {
+      var substring = this.slice(i,j)
+      if (res.indexOf(substring) === -1){
+        res.push(substring)
+      };
+    };
+  };
+  return res;
+}
